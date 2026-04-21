6af64562f7759cc7e08ca5017f742d233300dc21
30916 <ss10530211@g.ntsh.ntpc.edu.tw>
2026-04-21 10:41:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -41,3 +41,37 @@ def ai():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+#改成互動 4/14
+@app.route('/stock', methods=['GET', 'POST'])
+def stock():
+    if request.method == 'POST':
+        # 2. 讀取股票號碼
+        question = request.form.get('question', '').strip()
+        # 3. 查詢股票號碼的對應股價
+        answer = "抱歉，我目前沒有這個股票號碼。"
+        # 4. 回傳答案給使用者
+        return render_template('stock.html', question=question, answer=answer)
+    # GET 時給空白欄位
+    return render_template('stock.html', question="", answer="")
+
+
+@app.route('/leadership')
+def leadership():
+    return render_template('leadership.html')
+
+@app.route('/club')
+def club():
+    return render_template('club.html')
+
+@app.route('/electives')
+def electives():
+    return render_template('electives.html')
+
+@app.route('/ai')
+def ai():
+    return render_template('ai.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
